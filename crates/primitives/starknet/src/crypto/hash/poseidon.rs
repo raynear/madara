55f758ca3defedfa79b298d7d3d0489f1f4d0266
stompesi <stompesi@gmail.com>
2023-08-18 18:21:24 +0900

    Update hash functions

--- crates/primitives/starknet/src/crypto/hash/poseidon.rs
+++ crates/primitives/starknet/src/crypto/hash/poseidon.rs
@@ -1,7 +1,8 @@
 //! Poseidon hash module.
 use alloc::vec::Vec;
+use core::cmp;
 
-use starknet_crypto::{poseidon_hash, poseidon_hash_many, poseidon_hash_single, FieldElement};
+use starknet_crypto::{poseidon_hash, poseidon_hash_many, FieldElement};
 
 use crate::execution::felt252_wrapper::Felt252Wrapper;
 use crate::traits::hash::{DefaultHasher, HasherT};
@@ -18,11 +19,30 @@ impl HasherT for PoseidonHasher {
     /// # Returns
     /// The hash of the data.
     fn hash_bytes(&self, data: &[u8]) -> Felt252Wrapper {
-        let data = FieldElement::from_byte_slice_be(data).unwrap();
-        Felt252Wrapper(poseidon_hash_single(data))
+        // Calculate the number of 31-byte chunks we'll need, rounding up.
+        // (1 byte is used padding to prevent the value of field from being greater than modular)
+        // TODO: It is need a way to truncate bytes to fit into values smaller than modular(optimization)
+        let chunks = (data.len() + 30) / 31;
+
+        let mut data_vectors: Vec<Felt252Wrapper> = Vec::with_capacity(chunks);
+
+        for i in 0..chunks {
+            let start = i * 31;
+            let end = cmp::min(start + 31, data.len());
+
+            // Create a buffer for our 32-byte chunk.
+            let mut buffer = [0u8; 32];
+            buffer[1..end - start + 1].copy_from_slice(&data[start..end]);
+
+            // Convert the buffer to a FieldElement and then to a Felt252Wrapper.
+            let field_element = FieldElement::from_bytes_be(&buffer).unwrap();
+            data_vectors.push(Felt252Wrapper(field_element))
+        }
+
+        self.compute_hash_on_wrappers(&data_vectors)
     }
 
-    /// Hashes a slice of field elements using the Poseido hash function.
+    /// Hashes a slice of field elements using the Poseidon hash function.
     ///
     /// # Arguments
     ///
@@ -48,4 +68,22 @@ impl DefaultHasher for PoseidonHasher {
     fn hasher() -> Self {
         Self::default()
     }
+}
+
+#[test]
+fn dynamic_string_hashing() {
+    use core::str::FromStr;
+
+    let hasher = PoseidonHasher::hasher();
+
+    let message = format!("Hello, madara!!. It is poseidon hash."); // 37 bytes
+    let message = message.as_bytes();
+    let hash_value = hasher.hash_bytes(message);
+
+    assert_eq!(
+        hash_value,
+        Felt252Wrapper(
+            FieldElement::from_str("0x029b80231608c1cfcd7ff4aa3e7148fae5c16bf3c6e1b61a1034de7c0ac8469a").unwrap()
+        )
+    );
 }
