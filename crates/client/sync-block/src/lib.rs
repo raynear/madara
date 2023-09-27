d72f7d9c34140ad3b1fcf7165227f337d0288e6d
Noh Byung Kuk <raynear@gmail.com>
2023-09-27 10:34:35 +0900

    merge gylman's sync-block

--- crates/client/sync-block/src/lib.rs
+++ crates/client/sync-block/src/lib.rs
@@ -1,26 +1,18 @@
-// let a: Vec<_> = txs.encrypted_pool.values().cloned().collect();
-// println!("self.txs.get_mut: {:?}", a);
-
-use std::collections::HashMap;
 use std::path::Path;
-use std::str::Bytes;
-use std::time::Duration;
-use std::{env, str, thread, time};
+use std::{env, thread, time};
 
 use base64::engine::general_purpose;
 use base64::Engine as _;
-use bincode::{deserialize, serialize};
 use dotenv::dotenv;
-use hyper::header::{HeaderValue, AUTHORIZATION, CONTENT_TYPE};
-use hyper::{Body, Client, Request};
-use lazy_static::lazy::Lazy;
+use hyper::header::HeaderValue;
+use hyper::{body, Body, Client, Request, StatusCode};
 use lazy_static::lazy_static;
-use rocksdb::{Error, ErrorKind, IteratorMode, DB};
-use serde::de::DeserializeOwned;
-use serde::{Deserialize, Serialize};
+use rocksdb::{Error, IteratorMode, DB};
 use serde_json::{json, Value};
 use tokio;
-use tokio::runtime::Runtime; // Import Lazy from the lazy_static crate
+use tokio::runtime::Runtime;
+
+// Import Lazy from the lazy_static crate
 // Import the Error type from rocksdb crate
 // Define a struct to hold the DB instance.
 pub struct MyDatabase {
@@ -32,18 +24,13 @@ impl MyDatabase {
     fn open() -> Result<MyDatabase, Error> {
         let path = Path::new("epool");
         let db = DB::open_default(&path)?;
-        // let db = DB::open_default(&path).expect("Failed to open database");
         Ok(MyDatabase { db })
     }
 
     // Method to perform a read operation.
-    fn read<K, V>(&self, key: K) -> V
-    where
-        K: Serialize,
-        V: DeserializeOwned, // Ensure T can be deserialized.
-    {
+    fn read(&self, key: String) -> String {
         // Serialize key to bytes
-        let key_bytes = serialize(&key).unwrap();
+        let key_bytes = key.as_bytes();
 
         // Use the 'get' method to retrieve the value.
         let result_of_get = self.db.get(key_bytes);
@@ -57,83 +44,40 @@ impl MyDatabase {
         };
 
         // Handle the None case.
-        let my_vec = match option {
+        let value_vec = match option {
             Some(val) => val,
             None => Vec::new(),
         };
-        println!("RIGHT BEFORE DESER ERROR: my_vec: {:?}", my_vec);
-        // Deserialize the data into the desired type T.
-        let mut retries = 0;
-        let max_retries = 5;
-        let mut value: Option<V> = None;
-
-        while retries < max_retries {
-            match deserialize(&my_vec) {
-                Ok(val) => {
-                    value = Some(val);
-                    break; // Break the loop on successful deserialization
-                }
-                Err(err) => {
-                    retries += 1;
-                    eprintln!(
-                        "Failed to deserialize, the error is: {:?}, number of trials: {} of {}",
-                        err, retries, max_retries
-                    )
-                }
-            }
-        }
-        // If value is still None after retries, you can handle it as needed.
-        let value = value.unwrap_or_else(|| {
-            // Handle the case where deserialization still failed after max retries.
-            // You can return a default value or panic, depending on your use case.
-            panic!("Failed to deserialize after {} retries", max_retries);
-        });
+        let value = String::from_utf8(value_vec).unwrap();
 
         value
     }
 
     // Method to perform a write operation.
-    pub fn write<K, V>(&self, key: K, value: V)
-    where
-        K: Serialize,
-        V: Serialize,
-    {
+    pub fn write(&self, key: String, value: String) {
         // Serialize key to bytes
-        let key_bytes = serialize(&key).unwrap();
+        let key_bytes = key.as_bytes();
+
         // Serialize value for storing in DB
-        let value_bytes = serialize(&value).unwrap();
+        let value_bytes = value.as_bytes();
         let result_of_put = self.db.put(key_bytes, value_bytes);
         match result_of_put {
-            Ok(()) => {
-                println!("Successfully wrote to DB!");
-                self.display_all();
-                println!("Displaying inside Write function");
-            }
+            Ok(()) => {}
             Err(err) => eprintln!("Failed to write to DB: {:?}", err),
         };
     }
 
-    fn clear_db(&self) {
+    fn clear(&self) {
         // Create an iterator starting at the first key.
         let iter = self.db.iterator(IteratorMode::Start);
 
         // Iterate through all key-value pairs and print them.
         for result in iter {
-            match result {
-                Ok((key, value)) => {
-                    // println!("display_all_data: key: {:?} value {:?}", key, value);
-                    let deleted = self.db.delete(key);
-                    match deleted {
-                        Ok(()) => {
-                            println!("Deleted successfully")
-                        }
-                        Err(err) => {
-                            eprintln!("There is an error! {:?}", err);
-                        }
-                    }
-                }
+            let deleted = self.db.delete(result.unwrap().0);
+            match deleted {
+                Ok(()) => {}
                 Err(err) => {
-                    eprintln!("There is an error! {:?}", err);
+                    eprintln!("Failed to delete, error: {:?}", err);
                 }
             }
         }
@@ -148,7 +92,7 @@ impl MyDatabase {
             match result {
                 Ok((key, value)) => {
                     // println!("display_all_data: key: {:?} value {:?}", key, value);
-                    println!("display_all_data: key: {:?} value: {:?}", key, value.len());
+                    println!("display_all_data: key: {:?} value.len(): {:?}", key, value.len());
                 }
                 Err(err) => {
                     eprintln!("There is an error! {:?}", err);
@@ -157,36 +101,41 @@ impl MyDatabase {
         }
     }
 
-    fn get_next_entry<K>(&self, start_key: K) -> K
-    where
-        K: Serialize + DeserializeOwned,
-    {
+    fn get_next_entry(&self, start_key: String) -> (String, String) {
         // Serialize key to bytes. The process is 2-step since u64 does not directly support as_ref()
-        let serialized = serialize(&start_key).unwrap();
-        let key_bytes = serialized.as_ref();
+        let key_bytes = start_key.as_bytes();
 
         // Create an iterator starting from the key after the specified start_key.
         let mut iter = self.db.iterator(IteratorMode::From(key_bytes, rocksdb::Direction::Forward));
 
         // Iterate to get the next entry.
-        // Note: I am bravely unwrapping it because we would not iterate once we reach the last entry, since
-        // higher-level condition would not call this function in such case
-        let key = iter.next().unwrap().unwrap().0;
-        let key_vec = key.into_vec();
-        return deserialize(&key_vec).unwrap();
+        let key_vec = iter.next().unwrap().unwrap().0.into_vec();
+        let value_vec = iter.next().unwrap().unwrap().1.into_vec();
+        let key = String::from_utf8(key_vec).unwrap();
+        let value = String::from_utf8(value_vec).unwrap();
+        return (key, value);
     }
 }
 
 // Create a global instance of MyDatabase that can be accessed from other modules.
-// Create a global instance of MyDatabase that can be accessed from other modules.
 lazy_static! {
-    pub static ref SYNC_DB: MyDatabase = MyDatabase::open().unwrap_or_else(|err| {
-        eprintln!("Failed to open database: {:?}", err);
-        std::process::exit(1); // Exit the program on error
-    });
+    pub static ref SYNC_DB: MyDatabase = {
+        let db = MyDatabase::open().unwrap_or_else(|err| {
+            eprintln!("Failed to open database: {:?}", err);
+            std::process::exit(1); // Exit the program on error
+        });
+
+        db.clear();
+
+        // Perform write operations here
+        db.write("sync".to_string(), "0".to_string());
+        db.write("sync_target".to_string(), "0".to_string());
+
+        db // Return the initialized MyDatabase
+    };
 }
 
-fn encode_data_to_base64(original: &str) -> String {
+fn encode_data_to_base64(original: String) -> String {
     // Convert string to bytes
     let bytes = original.as_bytes();
     // Convert bytes to base64
@@ -194,71 +143,93 @@ fn encode_data_to_base64(original: &str) -> String {
     base64_str
 }
 
-async fn submit_to_da(data: &str) -> String {
+async fn submit_to_da(data: String) -> Result<String, Box<dyn std::error::Error>> {
     dotenv().ok();
-    let da_host = env::var("DA_HOST").expect("DA_HOST must be set");
-    let da_namespace = env::var("DA_NAMESPACE").expect("DA_NAMESPACE must be set");
-    let da_auth_token = env::var("DA_AUTH_TOKEN").expect("DA_AUTH_TOKEN must be set");
+    let da_host = env::var("DA_HOST")?;
+    let da_namespace = env::var("DA_NAMESPACE")?;
+    let da_auth_token = env::var("DA_AUTH_TOKEN")?;
     let da_auth = format!("Bearer {}", da_auth_token);
 
+    let encoded_data = encode_data_to_base64(data);
+
     let client = Client::new();
+
     let rpc_request = json!({
+        "id": 1,
         "jsonrpc": "2.0",
         "method": "blob.Submit",
         "params": [
             [
                 {
                     "namespace": da_namespace,
-                    "data": data,
+                    "data": encoded_data,
                 }
             ]
         ],
-        "id": 1,
     });
 
-    let uri = std::env::var("da_uri").unwrap_or(da_host.into());
+    // Create a mutable request builder
+    let request_builder = Request::builder()
+        .method("POST")
+        .uri(&da_host)
+        .header("Authorization", da_auth.clone()) // Clone da_auth here
+        .header("Content-Type", "application/json")
+        .header("timeout", HeaderValue::from_static("100"))
+        .body(Body::from(rpc_request.to_string()))?;
 
-    let req = Request::post(uri.as_str())
-        .header(AUTHORIZATION, HeaderValue::from_str(da_auth.as_str()).unwrap())
-        .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
-        .body(Body::from(rpc_request.to_string()))
-        .unwrap();
-    let response_future = client.request(req);
+    // Send the request and await the response
+    let response = client.request(request_builder).await?;
 
-    let resp = tokio::time::timeout(Duration::from_secs(100), response_future)
-        .await
-        .map_err(|_| "Request timed out")
-        .unwrap()
-        .unwrap();
+    if response.status() != StatusCode::OK {
+        return Err(format!("Request failed with status code: {}", response.status()).into());
+    }
 
-    let response_body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
-    let parsed: Value = serde_json::from_slice(&response_body).unwrap();
+    let response_body = body::to_bytes(response.into_body()).await?;
+    let parsed: Value = serde_json::from_slice(&response_body)?;
 
-    // println!("stompesi - {:?}", parsed);
-
-    if let Some(result_value) = parsed.get("result") { result_value.to_string() } else { "".to_string() }
+    if let Some(result_value) = parsed.get("result") {
+        Ok(result_value.to_string())
+    } else {
+        Err("Result not found in response".into()) // Or create a custom error type
+    }
 }
 
 pub fn sync_with_da() {
-    println!("**********sync_with_da STARTED!**********");
-
-    SYNC_DB.write("sync", "Hello World");
-
-    SYNC_DB.clear_db();
-    println!("Clearance");
-
-    SYNC_DB.display_all();
-    println!("First display after writing");
-
-    let three_seconds = time::Duration::from_millis(3000);
-
-    let mut txs: Vec<u8>;
-    let mut sync_target: u64 = 0;
-    let mut sync: u64 = 0;
-    let mut block_height: String = "".to_string();
-
-    SYNC_DB.write("sync", sync);
-    SYNC_DB.write("sync_target", sync_target);
+    println!(
+        "
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&####&&@@@@@@@#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@&P?777J5B&@@@@@#G5?!~^^::::::^^~!?5BJ!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@B: :7?7~^:^!J5?~::~7J5PGB####BGP5J7~  :75#@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@7  G@@@@@B7  .  7B@@@@@@@@@@@@@@@@@Y.55!:.~Y#@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@J  J@@@B7..7G&#57~!Y#@@@@@@@@@@@@@Y Y@@@&G7..7B@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@#:  5#7 :Y#@@@@@@&G?!7P&@@@@@@@@@Y ?@@@@@@@&Y: 7B@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@G. ...J&@@@@@@@@@@@&GJ!JB@@#PJJ7 ~@@@@@@@@@@&Y..Y@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@5   !&@@@@@@@@@@@@@@@&P??7.     .?&@@@@@@@@@@#~ !&@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@&! .  ^G@@@@@@@@@@@@@&&@@J         Y@@@@@@@@@@@@7 ~&@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@7 !G:   ?P5YYJJJJJYYYJJJY?         !YJYYY55PGBB#&7 !@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@&BP?  ^!~~    !GBB###&&&&&@@@@G:     .P&##BBGP5YJ?7!!~  ?PB&@@@@@@@@@@@@@
+        @@@@@@@@@&P?^::^  ?##&@5.   ~G@@@@@@@@@@@@@5. ?GBBJ?G@@@@@@@@@@@&&#J  ~^:~?P&@@@@@@@@@
+        @@@@@@@&J: ~JG&B..B@@@@@#!    !G@@@@@@@@@@Y  !@@@@@#J7G@@@@@@@@@@@@#. B&BY! :J&@@@@@@@
+        @@@@@@@7 .G@@@@P .#@@@@@@@5:    7B@@@@@@@Y  ^&@@@@@@@B?7G@@@@@@@@@@&: P@@@@B: 7@@@@@@@
+        @@@@@@@J  ?G&@@G .#@@@@@@@@&?.   .7B@@@@Y  :#@@@@@@@@@@G7?#@@@@@@@@&: G@@&B?  J@@@@@@@
+        @@@@@@@@P~  :!JY. G@@@@@@@@@@#7.   .7B@Y  .G@@@@@@@@@@@@@P!J&@@@@@@G  YY!:  ~P@@@@@@@@
+        @@@@@@@@@@BY!:    .^~7?JY5PGGBB5~    .~   Y@&&&&&&&####BBBP^^YYJ7!~:    :!YB@@@@@@@@@@
+        @@@@@@@@@@@@@&BPJ. ..          ..         :::::::::....          .. .JPB&@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@Y :GBP5J?!~^:...                    ..:^^~7?J7 ?G: J@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@J :B@@@@@@@&&##BB7   .     :?PGGGBB##&&@@@@@@Y.: J@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@5. Y@@@@@@@@@@@5   :BG7:   .!5&@@@@@@@@@@@@@5  :#@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@B~ ~G@@@@@@@@5   .G@@@#5!.   :7G&@@@@@@@@G~ !Y ^#@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@P^ ~5&@@@@Y    5@@@@@@@#Y~.   ^JG&@@&P~ ^5@@J ~&@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@&P!.:7P&5    J@@@@@@@@@@@#5!:   ^??:.!G@@@@&: 5@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@#Y~::    !@@@@@@@@@@@@@@&B?.     :JPB#&&G. Y@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@J     :~!7JJYYYYJJ7!^::^!JGGY7^.  .:: .?&@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@P    :BG5Y?77!~~!77?Y5G#&@@@@@@&#BGP55G#@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@G^..^B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@&##&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    "
+    );
 
     // Create the runtime
     let rt = match Runtime::new() {
@@ -273,48 +244,28 @@ pub fn sync_with_da() {
     };
 
     loop {
-        println!("SLEEPING FOR 3 SECONDS");
-        thread::sleep(three_seconds);
-        sync_target = SYNC_DB.read("sync_target");
-        sync = SYNC_DB.read("sync");
+        thread::sleep(time::Duration::from_millis(3000));
+        let sync = SYNC_DB.read("sync".to_string());
+        let sync_target = SYNC_DB.read("sync_target".to_string());
 
         println!("sync_target: {:?} and sync {:?}", sync_target, sync);
         if sync_target != sync {
             SYNC_DB.display_all();
-
-            println!("this is sync inside if: {:?}", sync);
-            println!("this is sync_target inside if: {:?}", sync_target);
-
-            let next_entry = SYNC_DB.get_next_entry(sync);
-            println!("this is after next_entry: {}", next_entry);
-
-            txs = SYNC_DB.read(next_entry);
-
-            println!("these are the txs: {:?}", txs);
-
-            // Check if the conversion was successful
-            let txs_str = match str::from_utf8(&txs) {
-                Ok(val) => {
-                    println!("Converted to str");
-                    val
-                }
-                Err(err) => {
-                    eprintln!("Conversion to str failed: {:?}", err);
-                    ""
-                }
-            };
-
-            println!("these are the txs_str: {:?}", txs_str);
-
-            let encoded_txs = encode_data_to_base64(txs_str);
-
+            let (next_sync, next_txs) = SYNC_DB.get_next_entry(sync);
             rt.block_on(async {
-                block_height = submit_to_da(&encoded_txs).await;
-                println!("this is the block height from DA: {}", block_height);
+                let block_height = submit_to_da(next_txs).await;
+                match block_height {
+                    Ok(block_height) => {
+                        println!(
+                            "<------------------------------------DA BLOCK \
+                             HEIGHT------------------------------------------>: {}",
+                            block_height
+                        );
+                        SYNC_DB.write("sync".to_string(), next_sync);
+                    }
+                    Err(err) => eprintln!("Failed to submit to DA with error: {:?}", err),
+                }
             });
-            if block_height.len() != 0 {
-                SYNC_DB.write("sync", next_entry);
-            }
         }
     }
 }
